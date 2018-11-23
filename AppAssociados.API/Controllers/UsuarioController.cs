b2831c2955046aa4d2ada856c50aba824ec56476
Marcoantonioo <marco.lentz2@gmail.com>
2018-11-23 15:24:12 -0200

    Entrega do trabalho de g2

---
+++ AppAssociados.API/Controllers/UsuarioController.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using AppAssociados.Domain;
+using AppAssociados.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AppAssociados.API.Controllers
+{
+    [Route("api/controller")]
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepository repository;
+
+        public UsuarioController(IUsuarioRepository repository) {
+            this.repository = repository;
+        }
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<Usuario> Get()
+        {
+            return this.repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public Usuario Get(int id)
+        {
+            return this.repository.GetById(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Usuario usuario)
+        {
+            this.repository.Create(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody]Usuario usuario)
+        {
+            this.repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            this.repository.Delete(id);
+            return Ok(new {
+                message = "Deletado com sucesso!"
+            });
+        }
+         [HttpPost("auth")]
+        public IActionResult Authentication([FromBody]Usuario user)
+        {
+            var usuario = this.repository.AuthUser(user);
+            if(usuario == null)
+                return BadRequest(new{
+                    message = "Login e/ou senha incorreto(s)."
+                });
+                return Ok(new{
+                    token = BuildToken()
+                });
+        }
+        public string BuildToken(){
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("UsuarioLogin"));
+            var creed = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                audience: "Usuario",
+                issuer: "Usuario",
+                signingCredentials: creed
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ AppAssociados.Domain/Usuario.cs
@@ -0,0 +1,12 @@
+namespace AppAssociados.Domain
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+        public string nome { get; set; }
+
+        
+    }
+}
